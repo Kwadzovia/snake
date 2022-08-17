8c4987d277a074b57435d2c2aefeb3c17cfb1d1e
kwadzovia <selikemkwadz@gmail.com>
2022-08-17 01:07:09 -0700

    fill out MAX7219.c

--- Core/Inc/MAX7219.h
+++ Core/Inc/MAX7219.h
@@ -4,12 +4,18 @@
 /* --------------------- Includes --------------------- */
 #include "spi.h"
 #include "stm32f4xx_hal_def.h"
+
 /* --------------------- Defines --------------------- */
+#define MAX7219_DEFAULT_INTENSITY 0x9
+#define MAX7219_DEFAULT_SCAN_LIMIT 0x7
+
+/* --------------------- Registers --------------------- */
 #define MAX7219_DECODE_MODE         0x9
 #define MAX7219_INTENSITY           0xA
 #define MAX7219_SCAN_LIMIT          0xB
 #define MAX7219_SHUTDOWN_REGISTER   0xC
 #define MAX7219_DISPLAY_TEST        0xF
+
 /* --------------------- Prototypes --------------------- */
 typedef struct 
 {
@@ -24,8 +30,9 @@ HAL_StatusTypeDef MAX7219_setIntensity(MAX7219 * dev, uint8_t intensity);
 HAL_StatusTypeDef MAX7219_setScanLimit(MAX7219 * dev, uint8_t scanLimit);
 HAL_StatusTypeDef MAX7219_shutdown(MAX7219 * dev);
 HAL_StatusTypeDef MAX7219_wake(MAX7219 * dev);
-HAL_StatusTypeDef MAX7219_displayTest(MAX7219 * dev);
+HAL_StatusTypeDef MAX7219_enable_displayTest(MAX7219 * dev);
+HAL_StatusTypeDef MAX7219_disable_displayTest(MAX7219 * dev);
 
-HAL_StatusTypeDef ReadRegisters(SPI_HandleTypeDef *hspi, uint8_t *pData, uint16_t Size, uint32_t Timeout);
-HAL_StatusTypeDef WriteRegister(SPI_HandleTypeDef *hspi, uint8_t *pData, uint16_t Size, uint32_t Timeout);
+HAL_StatusTypeDef ReadRegisters(SPI_HandleTypeDef *hspi, uint8_t *pData, uint16_t Size);
+HAL_StatusTypeDef WriteRegister(SPI_HandleTypeDef *hspi, uint8_t *pData, uint16_t Size);
 #endif /* __MAX7219_H__ */
---
+++ Core/Src/MAX7219.c
@@ -0,0 +1,82 @@
+/**
+  ******************************************************************************
+  * @file    max7219_driver.c
+  * @brief   This file provides code for the configuration an use of an 8x8 LED
+  *          Matrix Module
+  ******************************************************************************
+  * @attention
+  *
+  * Made by Selikem Kwadzovia
+  ******************************************************************************
+  */
+
+  /* --------------------- Includes --------------------- */
+  #include "MAX7219.h"
+  /* --------------------- Functions --------------------- */
+
+
+HAL_StatusTypeDef ReadRegisters(SPI_HandleTypeDef *hspi, uint8_t *pData, uint16_t Size)
+{
+  return HAL_SPI_Receive(hspi, pData, Size, HAL_MAX_DELAY);
+}
+
+HAL_StatusTypeDef WriteRegister(SPI_HandleTypeDef *hspi, uint8_t *pData, uint16_t Size)
+{
+  return HAL_SPI_Transmit(hspi, pData, Size, HAL_MAX_DELAY);
+}
+
+HAL_StatusTypeDef MAX7219_init(MAX7219 * dev, SPI_HandleTypeDef* spiHandle)
+{
+  MAX7219_shutdown(dev);
+  MAX7219_setIntensity(dev,MAX7219_DEFAULT_INTENSITY);
+  MAX7219_setScanLimit(dev,MAX7219_DEFAULT_SCAN_LIMIT);
+  MAX7219_disable_displayTest(dev);
+  return MAX7219_wake(dev);
+}
+
+HAL_StatusTypeDef MAX7219_setIntensity(MAX7219 * dev, uint8_t intensity)
+{
+  uint8_t intensityVal[2] = {MAX7219_INTENSITY,intensity};
+  return WriteRegister(dev->spiHandle, intensityVal, sizeof(intensityVal));
+}
+
+HAL_StatusTypeDef MAX7219_setScanLimit(MAX7219 * dev, uint8_t scanLimit)
+{
+  uint8_t scanVal[2] = {MAX7219_SCAN_LIMIT,scanLimit};
+  return WriteRegister(dev->spiHandle, scanVal, sizeof(scanVal));
+}
+
+HAL_StatusTypeDef MAX7219_shutdown(MAX7219 * dev)
+{
+  uint8_t scanVal[2] = {MAX7219_SHUTDOWN_REGISTER,0};
+  return WriteRegister(dev->spiHandle, scanVal, sizeof(scanVal));
+}
+
+HAL_StatusTypeDef MAX7219_wake(MAX7219 * dev)
+{
+  uint8_t scanVal[2] = {MAX7219_SHUTDOWN_REGISTER,1};
+  return WriteRegister(dev->spiHandle, scanVal, sizeof(scanVal));
+}
+
+HAL_StatusTypeDef MAX7219_enable_displayTest(MAX7219 * dev)
+{
+  uint8_t scanVal[2] = {MAX7219_DISPLAY_TEST,1};
+  return WriteRegister(dev->spiHandle, scanVal, sizeof(scanVal));
+}
+
+HAL_StatusTypeDef MAX7219_disable_displayTest(MAX7219 * dev)
+{
+  uint8_t scanVal[2] = {MAX7219_DISPLAY_TEST,0};
+  return WriteRegister(dev->spiHandle, scanVal, sizeof(scanVal));
+}
+
+HAL_StatusTypeDef MAX7219_setRow(MAX7219 * dev, uint8_t row, uint8_t value)
+{
+  if(row > 0 && row <= 8)
+  {
+    uint8_t scanVal[2] = {value,row};
+    return WriteRegister(dev->spiHandle, scanVal, sizeof(scanVal));
+  } else {
+    return HAL_ERROR;
+  }
+}
